76634f4d5fc3ed280f0e134a8650cbbcef2898ac
Janina Esins <NiniNinja@gmx.de>
2020-03-20 16:11:47 +0100

    minor adaptations

--- plot1.R
+++ plot1.R
@@ -7,6 +7,7 @@ png(file="plot1.png", width = 480, height = 480)
 #plot the histogramm
 hist(mydata$Global_active_power, col="red", 
      xlab ="Global Active Power (kilowatts)",
+     ylab ="Frequency",
      main = "Global Active Power")
 
 #close the png-file
