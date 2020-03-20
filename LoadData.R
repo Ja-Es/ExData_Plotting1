6acb5dfaeeb95e0673e9e549132d86f8a852bb43
Janina Esins <NiniNinja@gmx.de>
2020-03-20 16:23:29 +0100

    edit + comment

--- LoadData.R
+++ LoadData.R
@@ -17,5 +17,3 @@ names(mydata) = names(myheader)
 # change first column into date format
 mydata$Date <- as.Date(mydata$Date, "%d/%m/%Y")
 
-# replace possible missing values coded with "?" with "NA" instead
-mydata <- gsub("//?","NA",mydata)
