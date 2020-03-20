7513424010ed09b811043388cf15f2c1722cd1ff
Janina Esins <NiniNinja@gmx.de>
2020-03-20 16:05:07 +0100

    edited some comments

--- LoadData.R
+++ LoadData.R
@@ -4,9 +4,9 @@
 ## read only the lines containing data for 1/7/2007 and 2/7/2007
 library(data.table)
 mydata <- fread("./exdata_data_household_power_consumption/household_power_consumption.txt",
-                skip = "1/2/2007;00:00:00;", 
+                skip = "1/2/2007;00:00:00;", #start reading in lines from here
                 sep = ";",
-                nrow = 2880) # one line / min for 2 days = 2880 entries
+                nrow = 2880) # read 2880 lines (one line per minute for 2 days = 2880 lines)
 
 # With skippig, you have to read the header seperately
 myheader <- fread("./exdata_data_household_power_consumption/household_power_consumption.txt",
